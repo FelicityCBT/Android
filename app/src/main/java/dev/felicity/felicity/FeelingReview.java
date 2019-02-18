76e3eb35885f07a9f3b139c30b51122c14a4b4ce
philliplagoc <philliplagoc@gmail.com>
2019-02-18 13:34:28 -0800

    finished pros, cons, problematic patt display, and link to feedback

--- app/src/main/java/dev/felicity/felicity/FeelingReview.java
+++ app/src/main/java/dev/felicity/felicity/FeelingReview.java
@@ -42,9 +42,8 @@ public class FeelingReview  extends AppCompatActivity {
                     Toast.makeText(FeelingReview.this,"Fields cannot be empty",Toast.LENGTH_LONG).show();
                 }
                 else{
-                    // TODO: Change to ResponseOverview Page
-                    // Intent intentLoadNewActivity = new Intent(FeelingReview.this, ResponseOverview.class);
-                    Intent intentLoadNewActivity = new Intent(FeelingReview.this, PatternsReview.class);
+                    Intent intentLoadNewActivity = new Intent(FeelingReview.this, ResponseOverview.class);
+//                    Intent intentLoadNewActivity = new Intent(FeelingReview.this, PatternsReview.class);
                     mInfo.put("FeelingReview1",info2);
                     intentLoadNewActivity.putExtra("mInfo",mInfo);
                     startActivity(intentLoadNewActivity);
