ccdb8655be032572e39b67488f7a695b38c9307f
Phillip Lagoc <philliplagoc@gmail.com>
2019-02-25 20:05:21 -0800

    still fixing merge conflicts in Login

--- app/src/main/java/dev/felicity/felicity/Login.java
+++ app/src/main/java/dev/felicity/felicity/Login.java
@@ -21,7 +21,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.signin.SignIn;
+//import com.google.android.gms.signin.SignIn;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import android.text.TextUtils;
